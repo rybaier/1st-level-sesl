ed0288cb2da27c92e36c8fca12cba1e3536da856
rybaier <rybaier@gmail.com>
2023-04-11 15:15:50 -0700

    geoloc working need gmap api key

--- src/components/MapClicker.js
+++ src/components/MapClicker.js
@@ -1,19 +1,40 @@
 import React, { useEffect, useState } from "react";
+import { Button } from "reactstrap";
+import { BiCurrentLocation } from "react-icons/bi";
+
 
 const MapClicker = () => {
-    const [geoLoc, SetGeoLoc] = useState()
+    const [geoLoc, SetGeoLoc] = useState({})
     const [geoAvailable, SetGeoAvailable ] = useState(false)
+
+
 useEffect(() => {
     if ("geolocation" in navigator) {
         console.log("Available")
         SetGeoAvailable(true)
+        getLocation()
     } else {
         console.log("unavailable")
     }
 },[])    
 
+    const getLocation = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log("Latitude is:", position.coords.latitude)
+            console.log("Longitude is:", position.coords.longitude)   
+            SetGeoLoc({
+                lat: position.coords.latitude,
+                long: position.coords.longitude
+            })
+            console.log(geoLoc)
+        })
+        console.log(geoLoc)
+    }
     return(
-        <h2> MapClicker</h2>
+        <div className="mapclicker">
+           <h2> Add Your Location to the Locations Clicked</h2>
+           <Button onClick={() => {getLocation()}}><BiCurrentLocation size={'40px'}/> </Button>
+        </div>
     )
 }
 
